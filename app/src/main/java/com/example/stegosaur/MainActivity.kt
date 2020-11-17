bb23eb84a0f3eaf1838c41b47487037d3983716a
kylehustek <kylehustek@gmail.com>
2020-11-16 23:40:52 -0500

    Added gallery query intent, and display selected image

--- app/src/main/java/com/example/stegosaur/MainActivity.kt
+++ app/src/main/java/com/example/stegosaur/MainActivity.kt
@@ -22,8 +22,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val gallery: Button = findViewById(R.id.gallery_view)
-        gallery.setOnClickListener{ seeGallery() }
+
+        val gallery: Button = findViewById(R.id.to_gallery)
+        gallery.setOnClickListener{
+            val intent = Intent(this, Gallery::class.java)
+            startActivity(intent)
+        }
 
         val genQR: Button = findViewById(R.id.qr_gen)
         genQR.setOnClickListener{
@@ -33,44 +37,15 @@ class MainActivity : AppCompatActivity() {
 
         val save: Button = findViewById(R.id.save_file)
         save.setOnClickListener{ saveFile() }
-//
-//        val selectImage: Button = findViewById(R.id.select_label)
-//        saveButton.setOnClickListener{ selectImage() }
 
     }
 
-    fun seeGallery(){
-        Toast.makeText(this, "Implement Gallery Read", Toast.LENGTH_SHORT).show()
-    }
-
-
-//    private fun selectImage(){
-//
-//        var mediaList: LiveData<List<Picture>>
-//
-//        val images_uri = Media.EXTERNAL_CONTENT_URI
-//        val proj = arrayOf(Media._ID, Media.DISPLAY_NAME, Media.DATE_ADDED)
-//        val selection = null
-//        val selectionArgs = null
-//        val sortOrder = "${Media.DATE_ADDED} DESC"
-//
-//        val images_cursor = contentResolver.query(images_uri, proj, selection, selectionArgs, sortOrder)
-//
-//        if (images_cursor != null) {
-//            while (images_cursor.moveToNext()){
-//                val id = images_cursor.getLong(images_cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID))
-//                val name = images_cursor.getString(images_cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
-//                val pic = Picture(id, name)
-//
-//            }
-//        }
-//
-//
-//    }
 
     private fun saveFile() {
         Toast.makeText(this, "Implement File Write", Toast.LENGTH_SHORT).show()
     }
+
+
 }
 
 
